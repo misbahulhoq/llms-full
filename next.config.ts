4ed956135224f978d6a129ef70f27677f38a3592
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2026-06-10 10:41:23 +0600

    fix: uncomment basePath configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
-// const isProd = process.env.NODE_ENV === "production";
-// const repoName = "/llm-docs";
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "/llm-docs";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
-  // basePath: isProd ? repoName : "",
+  basePath: isProd ? repoName : "",
   images: {
     unoptimized: true,
   },
